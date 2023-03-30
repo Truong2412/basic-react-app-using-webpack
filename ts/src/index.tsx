2cd05e4a8ad975e35dd13f1eb751bb9219dc043a
Trương <truongpham2412.nd@gmail.com>
2023-03-30 15:24:15 +0700

    add base js and ts

---
+++ ts/src/index.tsx
@@ -0,0 +1,7 @@
+import "./index.css";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+
+const rootElement = document.querySelector("#root") as HTMLElement;
+const root = createRoot(rootElement);
+root.render(<App />);
